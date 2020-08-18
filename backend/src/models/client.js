736e843c592588504f9236cbad62dbbb3629aed9
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-18 20:57:32 +0200

    Change the class to the schema

--- backend/src/models/client.js
+++ backend/src/models/client.js
@@ -1,13 +1,14 @@
 const { v4: uuidv4 } = require('uuid');
 const Booking = require('./booking')
-class Client {
-    constructor(name, phone, email) {
-      this.name = name
-      this.phone = phone
-      this.email = email
-      const clientId = uuidv4();
-      this.clientId = clientId
-    }     
-}
+const mongoose = require('mongoose')
 
-module.exports = Client
+const clientSchema = new mongoose.Schema ({
+
+  name : String,
+  phone : String,
+  email : String,
+  clientId : Number,
+})
+
+
+module.exports = mongoose.model('Client' , clientSchema)
