117258b3825378e1a156e7a17aea13915a74a26d
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-18 20:57:15 +0200

    Change the class to schema

--- backend/src/models/booking.js
+++ backend/src/models/booking.js
@@ -1,19 +1,18 @@
 const { v4: uuidv4 } = require('uuid');
+const bookingSchema = require('mongoose')
 
-class Booking{
-    constructor(client, barber, date) { 
-        this.client = client       
-        this.barber = barber        
-        this.date = date
-        const bookingId = uuidv4();
-        this.bookingId = bookingId
-    } 
-    
-}
+const bookingSchema = new mongoose.Schema({
+    client: Object,
+    barber: Object,
+    date: Number,
+    bookingId : Number,
+})
+
+
 const findBookingsByClient = (client, bookings) => bookings.filter(booking => booking.client.clientId === client.clientId)
 
 const findBookingsByBarber = (barber, bookings) => bookings.filter(booking => booking.barber.id === barber.id)
 
 const cancelBooking = (bookings, bookingId) => bookings.filter(booking => booking.bookingId !== bookingId)
 
-module.exports = { Booking, findBookingsByClient, findBookingsByBarber, cancelBooking }
+module.exports = mongoose.model({ 'Booking', 'bookingSchema' },{'findBookingsByClient'}, {'findBookingsByBarber'},{ 'cancelBooking' })
