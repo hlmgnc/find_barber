0f9d796a2f5f785ec76704a69906478e56a5db01
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-24 17:57:12 +0200

    Create new connection for database

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose')
 
-const username = process.env.MONGODB_USERNAME
-const password = process.env.MONGODB_PASSWORD
-const dbName = process.env.MONGODB_DATABASE
 let connectionString = process.env.MONGODB_CONNECTION_STRING
 
 if (!connectionString) {
-  connectionString = `mongodb+srv://${username}:${password}@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
+  connectionString = `mongodb+srv://:@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
 }
 
 mongoose.set('debug', true)
