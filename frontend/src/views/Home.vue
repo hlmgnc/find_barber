272b115058a47d5b693fdad951545d3ad88c40d3
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-25 17:18:53 +0200

    Add pug package and request from backend with axios

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,27 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+<template lang="pug">
+  .home
+    HelloWorld(msg="Welcome to Find Barber App")
+    div(v-for="client in clients") {{ client.name }}
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from 'axios'
 
 export default {
   name: "Home",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      clients: []
+    }
+  },
+  async created() {
+      const clientsRequest = await axios.get('/api/clients')
+      this.clients = clientsRequest.data 
   }
 };
 </script>
