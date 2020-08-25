b35c0394df954f46e9fe4697e922d72783ee4e6d
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-25 14:47:41 +0200

    Add post and get requests

--- backend/src/routes/client.js
+++ backend/src/routes/client.js
@@ -24,18 +24,22 @@ router.get('/', async (req, res) => {
   res.send(await Client.find(query))
 })
 
+router.post('/', async (req, res ) => {
+    const createdClient = await Client.create(req.body)
+    res.send(createdClient)
+})
+
 router.get('initialize', async (req, res) => {
-
-
-    const cem = await Barber.create({ name :'Cem', address: 'Wilhelm Str 14 65800 Wiesbaden' })
-    const furkan = await Barber.create({ name :'Furkan', address: 'Mainzerland str 25 67300 Frankfurt am Main' })
-    const mihri = await Barber.create({ name: 'Mihri', address: 'Eckenheimer Tor 11 67900 Frankfurt am Main' })
 
     const hilmi = await Client.create({ name:'Hilmi', phone:'+491725005050', email:'hlmgnc@gmail.com' })
     const gaby = await Client.create({ name: 'Gaby', phone:'+491772333', email:'gaby@gaby.com' })
     const paloma = await Client.create({ name: 'Paloma', phone: '+491778880404', email: 'paloma@paloma.com' })
     const jing = await Client.create({ name: 'Jing', phone :'+490809696', email: 'jing@jing.com' })
     const alina = await Client.create({ name: 'Alina', phone: '+4916677443', email: 'alina@alina.com' })
+
+    const cem = await Barber.create({ name :'Cem', address: 'Wilhelm Str 14 65800 Wiesbaden' })
+    const furkan = await Barber.create({ name :'Furkan', address: 'Mainzerland str 25 67300 Frankfurt am Main' })
+    const mihri = await Barber.create({ name: 'Mihri', address: 'Eckenheimer Tor 11 67900 Frankfurt am Main' })
 
     await hilmi.book(cem)
     await hilmi.book(furkan)
@@ -45,11 +49,24 @@ router.get('initialize', async (req, res) => {
     res.sendStatus(200)
 })
 
+router.post('/:clientId/books' ,async (req, res) =>{
+  const client = await client.findById(req.params.clientId)
+  const barber = await barber.findById(req.params.barberId)
+
+  await client.book(barber)
+  res.sendStatus(200)
+})
+
 router.get('/:clientId', async (req, res) => {
   const client = await Client.findById(req.params.clientId)
 
   if (client) res.render('client', { client })
   else res.sendStatus(404)
+})
+
+router.get('/:clientId/json', async (req, res) => {
+  const client = await Client.findById(req.params.clientId)
+  res.send(client)
 })
 
 module.exports = router
