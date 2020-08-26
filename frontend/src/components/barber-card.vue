4dd7cc8b899e3aaf04014e165b3abee2ec800e40
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-26 22:12:30 +0200

    Create new component for barber

---
+++ frontend/src/components/barber-card.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'BarberCard',
+    props: ['barber']
+}
+</script>
+
+<template lang="pug">
+.barber_box
+    h2 {{ barber.name }}    
+</template>
+
+<style lang="scss" scoped>
+.barber_box{
+    padding: 2em;
+    border: 2px solid rgb(33, 97, 182);
+    border-radius: 0.3em;
+}
+</style>
