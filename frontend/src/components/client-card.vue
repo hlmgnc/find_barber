329dfb5a1e6de1a0b7c597f4867fd7a6e6794f8e
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-26 22:12:47 +0200

    Create new component for client

---
+++ frontend/src/components/client-card.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    name: 'ClientCard',
+    props: ['client']
+}
+</script>
+
+<template lang="pug">
+.client_box
+    h2 {{ client.name }} 
+
+    .info(v-for="client in clients")
+    h3 {{ client.email }}
+
+</template>
+
+<style lang="scss" scoped>
+.client_box{
+    padding: 2em;
+    border: 2px solid rgb(33, 97, 182);
+    border-radius: 0.3em;
+}
+</style>
