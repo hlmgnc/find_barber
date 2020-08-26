c057bef301639d9937fb5122ec1df26a929cef27
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-26 22:11:17 +0200

    Create new connection string

--- backend/src/database-connection.js
+++ backend/src/database-connection.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 let connectionString = process.env.MONGODB_CONNECTION_STRING
 
-if (!connectionString) {
-  connectionString = `mongodb+srv://:@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
-}
-
+// if (!connectionString) {
+//   connectionString = `mongodb+srv://:@cluster0.knvjh.gcp.mongodb.net/${dbName}?retryWrites=true&w=majority`
+// }
+console.log(connectionString)
 mongoose.set('debug', true)
 
 mongoose
