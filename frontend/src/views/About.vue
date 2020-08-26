736f56041b7126c3660c1cdb4031487918884aee
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-08-26 22:15:21 +0200

    Create view files for about page with component

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
-    <h1>Welcome to the barbers page</h1>
+    <h1>Barbers Page</h1>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import ClientCard from '@/components/barber-card.vue'
+
+export default {
+  name: "About",
+  components: {
+    BarberCard
+  },
+  data() {
+    return {
+      barbers: []
+    }
+  },
+  async created() {
+      const barbersRequest = await axios.get('/api/barbers')
+      this.barbers = barbersRequest.data 
+  },
+
+};
+</script>
