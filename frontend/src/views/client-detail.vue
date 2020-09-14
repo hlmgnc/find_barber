c33dab832f65ffad5b85e77919da758ea087b442
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-09-14 18:48:19 +0200

    Add views for client-detail

---
+++ frontend/src/views/client-detail.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  .barber
+    h1 BOOKINGS
+    ClientCard(:client="client" v-if="client")
+</template>
+
+<script>
+import ClientCard from '@/components/client-card.vue'
+import { mapActions } from 'vuex'
+
+export default {
+  name: "ClientDetail",
+  components: { ClientCard },
+  data() {
+    return {
+      client: null
+    }
+  },
+  async created() {
+    this.clients = await this.fetchClient(this.$route.params.id) 
+  },
+  methods: {
+    ...mapActions(['fetchClient'])
+  }
+
+};
+</script>
