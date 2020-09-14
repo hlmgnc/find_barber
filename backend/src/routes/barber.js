ea0476d9b9776fbbdebde5754a40306595bf07f4
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-09-14 18:43:22 +0200

    Add new router

--- backend/src/routes/barber.js
+++ backend/src/routes/barber.js
@@ -3,6 +3,11 @@ const express = require('express')
 const router = express.Router()
 const Barber = require('../models/barber')
 
+router.get('/', async (req, res) => {
+  const barbers = await Barber.find({})
+  res.send(barbers)
+})
+
 router.post('/', async (req, res) => {
   const createdBooking = await PushSubscriptionOptions.create(req.body)
   res.send(createdBooking)
