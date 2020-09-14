03be9f8500875c192a57543163c66b04d7ced6cb
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-09-14 18:46:33 +0200

    Add some features to clientcard

--- frontend/src/components/client-card.vue
+++ frontend/src/components/client-card.vue
@@ -1,23 +1,28 @@
 <script>
 export default {
     name: 'ClientCard',
-    props: ['client']
+    props: ['client'],
+    data() {
+        return {
+            bookings: []
+        }
+    }
 }
 </script>
 
 <template lang="pug">
 .client_box
-    h2 {{ client.name }} 
-
-    .info(v-for="client in clients")
-    h3 {{ client.email }}
+    h1 {{ client.name }} 
+    .info(v-for="booking in client.bookings")
+        p {{ booking }}
+    h2 {{ client.email }}
 
 </template>
 
 <style lang="scss" scoped>
 .client_box{
     padding: 2em;
-    border: 2px solid rgb(33, 97, 182);
+    border: 1px solid rgb(33, 97, 182);
     border-radius: 0.3em;
 }
 </style>
