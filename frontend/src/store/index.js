1137fd0b8cacd8a803044e9b84b7314e336ef4e7
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-09-14 18:47:29 +0200

    Add actions to the index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {},
+  actions: {
+    async fetchClient(store, id){
+      const clientRequest = await axios.get(`/api/clients/${id}`)
+      return clientRequest.data 
+    },
+    async fetchClients(store) {
+      const clientRequest = await axios.get(`/api/clients`)
+      return clientRequest.data 
+    }
+  },
   modules: {}
 });
