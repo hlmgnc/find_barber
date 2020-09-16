5e523b5e47adc936bab4fc2a77103b848ac3efb3
hilmi genc <hilmigenc@Tugces-MBP.fritz.box>
2020-09-16 15:48:38 +0200

    Add api's for having session

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,12 +4,17 @@ const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const passport = require('passport')
+const User = require('./models/client')
 
-require('./database-connection')
+const mongooseConnection = require('./database-connection')
 
 const indexRouter = require('./routes/index')
 const clientsRouter = require('./routes/client')
 const barbersRouter = require('./routes/barber')
+const accountsRouter = require('./routes/accounts')
 
 const app = express()
 
@@ -30,9 +35,35 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+
+app.use(
+  session({
+    secret: ['thisisasupersecuresecretsecret', 'thisisnotasupersecuresecretsecret'],
+    store: new MongoStore({ mongooseConnection, stringify: false }),
+    cookie :{
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      path: '/api',
+    },
+  })
+)
+
+app.use(passport.initialize())
+app.use(passport.session())
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/api',(req,res,next) => {
+  req.session.viewCount = req.session.viewCount || 0
+  req.session.viewCount++
+  next()
+})
+
 app.use('/api', indexRouter)
+app.use('/api/account', accountsRouter)
 app.use('/api/clients', clientsRouter)
 app.use('/api/barbers', barbersRouter)
 
